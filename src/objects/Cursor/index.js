138d683991c1a7ad03c8bae2791ffb8dfa831cf3
Kirill Fyodorov <kirill.fyodorov@jetrockets.ru>
2020-08-18 22:52:47 +0300

    Change cursor color

--- src/objects/Cursor/index.js
+++ src/objects/Cursor/index.js
@@ -1,7 +1,7 @@
 import BaseObject from '../BaseObject/index';
 
 export default class Cursor extends BaseObject {
-  constructor({ x, y, color = 'green' }) {
+  constructor({ x, y, color = 'lightblue' }) {
     const width = 5;
     const height = 5;
     super({ x, y, width, height, color });
